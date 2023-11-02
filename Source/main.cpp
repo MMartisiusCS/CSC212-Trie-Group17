a6a83a1244c2f25f514a59f6fac1e80039bb902d
nlichtenfels <113181255+nlichtenfels@users.noreply.github.com>
2023-11-02 15:15:18 -0400

    Added basic file reading function

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,28 @@
 #include "trie.h"
-void outputDOTfile();
+
+//Set up trie with txt file
+Trietree fileSetup(std::string fileName) {
+    //Create: Section that creates a file reader, reads each line from the file and loads it into the tree.
+    std::ifstream infile;
+    std::string line;
+    infile.open(fileName);
+
+    Trietree tree;
+
+    if (infile.fail()) {
+        std::cerr << "Can't open file\n";
+        return 1;
+    }
+
+    while(std::getline(infile,line)) {
+        tree.insert(line, true);
+    }
+}
+
 int main(int argc, char*argv[]){
     //Add: Time measurment of time to create and load tree, and time to output DOT file?
-    //Create: Section that creates a file reader, reads each line from the file and loads it into the tree.
+    
+
     TrieTree tree;
     //Remove: These test inserts
     tree.insert("a",false);

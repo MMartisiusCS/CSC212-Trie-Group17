8e241b93942e80ab10f09c274e0c6d73578f191c
nlichtenfels <113181255+nlichtenfels@users.noreply.github.com>
2023-11-20 19:30:34 -0500

    small fix for autocomplete case

--- Source/main.cpp
+++ Source/main.cpp
@@ -192,7 +192,7 @@ int main(int argc, char*argv[]){
                         }
                     }
                     
-                    autocomplete(suffix, depth);
+                tree->autocomplete(inputText, depth);
                 break;
             }
             default:

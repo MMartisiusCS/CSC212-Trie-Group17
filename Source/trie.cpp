b8eb88c6b820b4ed49da5682fe55866ff7f1d732
nlichtenfels <113181255+nlichtenfels@users.noreply.github.com>
2023-11-28 15:00:01 -0500

    autocomplete bugfix

--- Source/trie.cpp
+++ Source/trie.cpp
@@ -219,15 +219,15 @@ void TrieTree::autocompleteHelper(TrieNode* node, int length, std::string curren
 void TrieTree::autocomplete(std::string suffix, int length) {
     // Search for suffix in the tree, starting at the end node of the suffix path
     TrieNode* startNode = search(suffix, root);
-
+    /*
     // If the suffix is not found, or the length is invalid, return
     if (startNode == nullptr || length < suffix.length()) {
         std::cout << "No words found." << std::endl;
         return;
-    }
+    } */
 
     // Call the recursive helper function to find words of the specified length
-    autocompleteHelper(startNode, length - suffix.length(), suffix);
+    autocompleteHelper(startNode, length, suffix);
 }
 
 int TrieTree::getWords()
